c1df5ea82c0691c1f2e0481dab4c27a69ca6b3e8
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2024-04-13 15:49:18 +0200

    added testimonials section

--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -2,5 +2,6 @@ import about from './about'
 import contact from './contact'
 import events from './events'
 import bidrar from './bidrar'
+import testimonials from './testimonials'
 
-export const schemaTypes = [about, contact, events, bidrar]
+export const schemaTypes = [about, contact, events, bidrar, testimonials]
