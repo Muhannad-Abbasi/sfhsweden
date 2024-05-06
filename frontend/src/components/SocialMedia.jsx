088a794dd713578d6a8aaf831f1521a562aa0e0c
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2024-05-06 19:10:48 +0200

    Update SocialMedia.jsx

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -8,7 +8,7 @@ const SocialMedia = () => (
       <a href='https://www.facebook.com/profile.php?id=61553978129354' target='_blank' rel="noreferrer"><FaFacebookF /></a>
     </div>
     <div>
-      <a href='https://www.tiktok.com/@solidarity.for.humanity' target='_blank' rel="noreferrer"><BsInstagram /></a>
+      <a href='https://www.instagram.com/solidarity.for.humanity/' target='_blank' rel="noreferrer"><BsInstagram /></a>
     </div>
     <div>
       <a href='https://www.tiktok.com/@solidarity.for.humanity' target='_blank' rel="noreferrer"><FaTiktok /></a>
