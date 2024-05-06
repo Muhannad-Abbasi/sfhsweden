2dd2857a943fcdab42e4dc68a4855509e290a99d
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2024-05-06 16:29:32 +0200

    Update SocialMedia.jsx

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -5,13 +5,13 @@ import { FaFacebookF, FaTiktok } from 'react-icons/fa';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a href='https://www.facebook.com/profile.php?id=61553978129354' target='_blank'><FaFacebookF /></a>
+      <a href='https://www.facebook.com/profile.php?id=61553978129354' target='_blank' rel="noreferrer"><FaFacebookF /></a>
     </div>
     <div>
-      <a href='https://www.instagram.com/solidarity.for.humanity/' target='_blank'><BsInstagram /></a>
+      <a href='https://www.instagram.com/solidarity.for.humanity/' target='_blank' rel="noreferrer"><BsInstagram /></a>
     </div>
     <div>
-      <a href='https://www.tiktok.com/@solidarity.for.humanity' target='_blank'><FaTiktok /></a>
+      <a href='https://www.tiktok.com/@solidarity.for.humanity' target='_blank' rel="noreferrer"><FaTiktok /></a>
     </div>
   </div>
 );
